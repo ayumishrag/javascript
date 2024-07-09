db7ffd962a228c58f9d686d355c5c7f7190cabc1
Ayushi Mishra <168451542+ayumishrag@users.noreply.github.com>
2024-07-09 04:46:03 +0000

    container config done

---
+++ basics/variable.js
@@ -0,0 +1,5 @@
+const accountId = 1223344
+let accountEmail = "ayu9935gmail";
+var accountPassword = '12345';
+accountCity = "Pratapgarah";
+console.table([accountId, accountEmail, accountPassword,accountCity]);
