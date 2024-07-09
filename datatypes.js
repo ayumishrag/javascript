f6ff4f06345446a6fe4e4ff1ff5319c7db7058c8
Ayushi Mishra <168451542+ayumishrag@users.noreply.github.com>
2024-07-09 05:30:12 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+//"use strict" //treat all the code as a newer JS version
+//alert(3+3) // here we can't use this in js , it has some diff rule to use this in node in browser we can run it.
+
+
+//datatypes in Js
+
+let name = "Ayushi"  //string 
+let age = 20         //numbers
+let isLoggedIn = true  // boolean
+
+
+//bigint //used in big industries and stock markets
+
+//null // standalone value.  this can be empty  this is an object
+
+// undefined it means that value is not defined yet
+
+// symbol mostly used in react 
+//object
+
+console.log(typeof age) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+
+
+ 
+
